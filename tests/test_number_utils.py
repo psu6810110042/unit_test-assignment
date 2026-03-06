1b16eafd991b4ee9459ca37ffc250842016ca08f
Jirakorn Sukmee <6810110042@psu.ac.th>
2026-03-06 12:56:26 +0700

    added: more test cases, changed how is_prime_number is done, 1 is not a prime number

--- tests/test_number_utils.py
+++ tests/test_number_utils.py
@@ -4,7 +4,32 @@ import unittest
 
 
 class PrimeListTest(unittest.TestCase):
-    def test_give_1_2_3_is_prime(self):
+    def test_give_1_2_3_5_is_not_prime(self):
         prime_list = [1, 2, 3, 5]
         is_prime = is_prime_list(prime_list)
+        self.assertFalse(is_prime)
+
+    def test_all_valid_primes(self):
+        prime_list = [2, 3, 5, 7, 11, 13]
+        is_prime = is_prime_list(prime_list)
         self.assertTrue(is_prime)
+
+    def test_all_composite_numbers(self):
+        prime_list = [4, 6, 8, 9, 10]
+        is_prime = is_prime_list(prime_list)
+        self.assertFalse(is_prime)
+
+    def test_mixed_primes_and_composites(self):
+        prime_list = [2, 3, 4, 5]
+        is_prime = is_prime_list(prime_list)
+        self.assertFalse(is_prime)
+
+    def test_empty_list(self):
+        prime_list = []
+        is_prime = is_prime_list(prime_list)
+        self.assertTrue(is_prime)
+
+    def test_zero_and_negatives(self):
+        prime_list = [0, -5, -7, 2]
+        is_prime = is_prime_list(prime_list)
+        self.assertFalse(is_prime)
