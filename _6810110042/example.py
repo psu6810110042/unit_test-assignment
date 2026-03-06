8dcea434c0a5416ebb3b00823d872d3fa11640f1
Jirakorn Sukmee <6810110042@psu.ac.th>
2026-03-06 14:39:47 +0700

    added: test double examples + tests, changed them to be 100% coverage and works with the project (directory names)

---
+++ _6810110042/example.py
@@ -0,0 +1,18 @@
+import random
+
+
+def random_integer():
+    print("step into random_integer")
+    return random.randint(0, 20)
+
+
+def is_even_number():
+    # 0 - 9
+    num = random_integer()
+    print("num is", num)
+    if num % 2 == 0:
+        return True
+    # else:
+    #     return False
+
+    return False
---
+++ tests/test_example.py
@@ -0,0 +1,31 @@
+import _6810110042.example as example
+import unittest
+from unittest import mock
+
+
+class RandomNumberTest(unittest.TestCase):
+    def test_random_should_0_20(self):
+        result = example.random_integer()
+
+        self.assertIn(result, range(0, 21))
+
+
+class IsEvenNumberTest(unittest.TestCase):
+    @mock.patch("_6810110042.example.random_integer", return_value=2)
+    def test_random_give_2_should_return_true(self, random_integer_mock):
+        result = example.is_even_number()
+
+        self.assertTrue(result)
+
+    @mock.patch("_6810110042.example.random_integer", return_value=5)
+    def test_random_give_5_should_return_false(self, random_integer_mock):
+        result = example.is_even_number()
+
+        self.assertFalse(result)
+
+    def test_random_should_return_true_or_false(self):
+        expected_result = [True, False]
+
+        result = example.is_even_number()
+
+        self.assertIn(result, expected_result)
