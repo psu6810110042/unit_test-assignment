9c8f42dab2eeb9f7579659607abb0fdf59dee193
Jirakorn Sukmee <6810110042@psu.ac.th>
2026-03-06 12:51:48 +0700

    slide work: add 5 to the is_prime_list test case

--- tests/test_number_utils.py
+++ tests/test_number_utils.py
@@ -1,8 +1,10 @@
 from _6810110042.number_utils import is_prime_list
 
-import unittest 
-class PrimeListTest(unittest.TestCase): 
-    def test_give_1_2_3_is_prime(self): 
-        prime_list = [1, 2, 3] 
-        is_prime = is_prime_list(prime_list) 
+import unittest
+
+
+class PrimeListTest(unittest.TestCase):
+    def test_give_1_2_3_is_prime(self):
+        prime_list = [1, 2, 3, 5]
+        is_prime = is_prime_list(prime_list)
         self.assertTrue(is_prime)
